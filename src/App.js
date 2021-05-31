6822fa8691dc51130256b393b1b092ce1b756d86
maecha <hiiamseiya@gmail.com>
2021-05-31 15:08:29 +0900

    [update] Setup GraphQL Client

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import React, { Component } from 'react'
+import { ApolloProvider } from 'react-apollo'
+import gql from 'graphql-tag'
+import { Query } from 'react-apollo'
+import client from './client'
+
+const ME = gql`
+  query me {
+    user(login: "maecha") {
+      name
+      avatarUrl
+    }
+  }
+`
 
 class App extends Component {
   render() {
     return (
-      <div>hello</div>
+      <ApolloProvider client={client}>
+        <Query query={ME}>
+          {
+            ({ loading, error, data }) => {
+              if (loading) return "Loading..."
+              if (error) return `ERROR! ${error.message}`
+
+              return <div>{data.user.name}</div>
+            }
+          }
+        </Query>
+      </ApolloProvider>
     )
   }
 }
