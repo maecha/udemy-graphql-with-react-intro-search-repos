822b74935001680a2cd62c26eeb81217c7fb14fa
maecha <hiiamseiya@gmail.com>
2021-06-04 09:52:07 +0900

    [update] Display stars

--- src/graphql.js
+++ src/graphql.js
@@ -49,3 +49,25 @@ export const ME = gql`
     }s
   }
 `
+
+export const ADD_STAR = gql`
+  mutation addStar($input: AddStarInput!) {
+    addStar(input: $input) {
+      starrable {
+        id
+        viewerHasStarred
+      }
+    }
+  }
+`
+
+export const REMOVE_STAR = gql`
+  mutation removeStar($input: RemoveStarInput!) {
+    removeStar(input: $input) {
+      starrable {
+        id
+        viewerHasStarred
+      }
+    }
+  }
+`
